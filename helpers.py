3fff0c95886d845371074c87d61f916e6f645e7e
Kirill Kiselev <kirillkiselev@Kirills-MacBook-Pro.local>
2024-03-30 00:53:16 +0300

    finilazed code with passed tests vol3

--- helpers.py
+++ helpers.py
@@ -4,12 +4,14 @@ TOKEN_NAMES = ['PRACTICUM_TOKEN', 'TELEGRAM_TOKEN', 'TELEGRAM_CHAT_ID']
 
 
 def compare_statuses(status_before, status_after):
+    """Проверяет, изменился ли статус выполнения задания."""
     if status_before == status_after:
         raise StatusDidNotChangeError(f'Статус работы не изменился...'
                                       f'Все еще "{status_after}"\uE108')
 
 
 def check_each_token(tokens: list):
+    """Проверяет наличие каждого токена."""
     for token, token_name in zip(tokens, TOKEN_NAMES):
         if token is None:
             raise TokenNotPresentError(f'Отсутствует обязательная '
